acbb338117b3c510b8a8a694cc268d566079658e
Shokunin San <me@flatmap.io>
2015-01-24 20:21:03 +0100

    Implemented the gradient descent algorithm.

--- mlclass-ex1-008/mlclass-ex1/gradientDescent.m
+++ mlclass-ex1-008/mlclass-ex1/gradientDescent.m
@@ -17,11 +17,32 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    % This vector of ones is used for calculation of theta zero
+    _ones = ones(m, 1);
 
+    % Extract the features/input
+    _x = X(:,2);
 
+    % Our vectorized hypothesis function
+    _h = X * theta;
 
+    % This is the derivative of the MSE functions.
+    % For theta zero, x is a vector of ones which keeps the values unchanged.
+    % For theta one, x is the feature vetor.
+    derivative = @(x) ((1/m) * sum((_h-y) .* x));
 
+    % The heart of the gradient descent algorithm, where alpha is the step size
+    % towards the global optimum (minimum) and the derivative function determines
+    % the steepest slope, which is the direction we head towards.
+    _theta_0 = theta(1) .- alpha * derivative(_ones);
+    _theta_1 = theta(2) .- alpha * derivative(_x);
 
+    theta = [_theta_0; _theta_1];
+
+    % For debugging purposes
+    %theta
+    %fprintf('cost = %f\n', computeCost(X, y, theta));
+    %fprintf('---------------\n');
 
     % ============================================================
 
