497d2759467547e58478f96e2d805169944f93c0
Shokunin San <me@flatmap.io>
2015-02-10 21:18:50 +0100

    Added cost function and gradient descent for logistic regression.

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -20,12 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = @(X) sigmoid(X*theta);
 
+J = (1/m)*sum(-y.*log(h(X))-(1.-y).*log(1.-h(X)));
 
-
-
-
-
+grad = (1/m)*sum((h(X).-y).*X);
 
 % =============================================================
 
