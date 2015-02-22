dd057baf79b60b02cebb53b344b22e3ae65693e6
Shokunin San <me@flatmap.io>
2015-02-22 18:35:42 +0100

    Ex3 2.2 Implemented ANN feedforward propagation and prediction.

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -21,13 +21,17 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m,1) X];
 
+z2 = a1 * Theta1';
 
+a2 = [ones(m,1) sigmoid(z2)];
 
+z3 = a2 * Theta2';
 
+a3 = sigmoid(z3);
 
-
-
+[x p] = max(a3, [], 2);
 
 % =========================================================================
 
