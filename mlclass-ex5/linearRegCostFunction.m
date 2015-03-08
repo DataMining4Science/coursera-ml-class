f49672d4208d024df45732049b095d0f0e89dd83
Shokunin San <me@flatmap.io>
2015-03-08 15:55:03 +0100

    Ex5 1.2 Implemented regularized linear regression cost function.

--- mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5/linearRegCostFunction.m
@@ -19,16 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+h = X*theta;
 
+J = (1/(2*m))*sum((h-y).^2) + (lambda/(2*m))*sum(theta(2:end,:).^2);
 
-
-
-
-
-
-
-
-
+grad = (1/m) * sum(X .* (h-y));
 
 % =========================================================================
 
